21404aac6694e06181ba57c6dce59e4e43bf339c
marco.vincenzi.antonio@gmail.com <marco.vincenzi.antonio@gmail.com>
2025-02-09 18:45:28 -0300

    First commit, created project and config

---
+++ src/main/java/com/paymentAPI/paypal_integration_api/PaypalIntegrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.paymentAPI.paypal_integration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalIntegrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalIntegrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentAPI/paypal_integration_api/PaypalIntegrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentAPI.paypal_integration_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalIntegrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
